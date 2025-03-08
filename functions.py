de931cccc2bf22c41d9202b1bd891e9ecd18f60c
Ber Mira <bamg359@gmail.com>
2025-03-08 09:01:37 -0500

    us-001/create_register firts commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+
+
+
+
+
+#US-001/create_register
+
+user = []
+
+
+def user_register():
+    id = int(input("Agregue su id"))
+    user.append(id)
+    name = input("Agregue su nombre")
+    user.append(name)
+    last_name = input("Agregue su apellido")
+    user.append(last_name)
+    email = input("Agregue su email")
+    user.append(email)
+    password = input("Cree un password de 8 caracteres entre numeros y letras")
+    user.append(password)
+    print("Usuario creado exitosamente")
+    print(user)
